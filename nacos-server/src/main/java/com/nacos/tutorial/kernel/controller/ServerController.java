a8fc3eaf7cc9479c956080f9fb95bf4c53b30038
19zfl <19aleiya@gmail.com>
2023-11-01 09:41:28 +0800

    nacos-config-center's config & controller java file update

--- nacos-server/src/main/java/com/nacos/tutorial/kernel/controller/ServerController.java
+++ nacos-server/src/main/java/com/nacos/tutorial/kernel/controller/ServerController.java
@@ -2,10 +2,13 @@ package com.nacos.tutorial.kernel.controller;
 
 import com.nacos.tutorial.kernel.pojo.Person;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sound.sampled.Port;
 
 /**
  * @author: 19zfl
@@ -14,14 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/server")
+@RefreshScope // 动态加载配置文件
 public class ServerController {
 
     @Value("${server.port}")
     private String port;
 
+    @Value("${temp.value}")
+    private String value;
+
     @GetMapping("/getPersonInfoById/{id}")
     public Person getPersonInfoById(@PathVariable Long id) {
-        return new Person(id, "刘十三", "地址在：" + port);
+        return new Person(id, "temp:" + value, "port：" + port);
     }
 
 }
